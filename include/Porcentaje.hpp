7b1d48959c76fc8484a29f4aaad07fdc7403248d
RodriJimenez <a23310312@live.ceti.mx>
2024-10-15 00:21:48 +0000

    clase 1

---
+++ include/Porcentaje.hpp
@@ -0,0 +1,27 @@
+class Porcentaje
+{
+private:
+    int numero;
+
+public:
+    Porcentaje()
+    {
+        this->numero = 0;
+    }
+    Porcentaje(int numero)
+    {
+        if (numero > 100 || numero < 100)
+        {
+            /* code */
+        }
+        else
+        {
+            /* code */
+        }
+    }
+    ~Porcentaje()
+    {
+    }
+    void Incrementar(int numero) {}
+    void Disminuir(int numero) {}
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Hola putos"<<std::endl;
+
+}
+
